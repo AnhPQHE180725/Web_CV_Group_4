1187551d7d5dad651331583e0732265764b114b8
AnhPQHE180725 <anhpqhe180725@fpt.edu.vn>
2025-03-20 15:58:06 +0700

    FrontEndRelease

--- Web_Client/src/app/app.routes.ts
+++ Web_Client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RecruitmentListComponent } from './feature/recruitment/recruitment-list/recruitment-list.component';
 import { HomepageComponent } from './feature/homepage/homepage.component';
+import { SignuppageComponent } from './feature/signuppage/signuppage.component';
+import { LoginpageComponent } from './feature/loginpage/loginpage.component';
 
 export const routes: Routes = [
     { path: 'recruitment/category/:id', component: RecruitmentListComponent },
@@ -8,4 +10,10 @@ export const routes: Routes = [
     {
         path: 'home', component: HomepageComponent
     },
+    {
+        path:'register',component: SignuppageComponent
+    },
+    {
+        path:'login', component: LoginpageComponent
+    }
 ];
