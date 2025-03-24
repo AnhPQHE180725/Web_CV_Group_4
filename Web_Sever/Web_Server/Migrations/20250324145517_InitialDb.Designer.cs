7a948779adf638f7e7ad15f5c14f8aaff164bad9
NguyenTranTrongHieu <tronghieutronghieu11@gmail.com>
2025-03-24 21:56:32 +0700

    fix migration

--- Web_Sever/Web_Server/Migrations/20250324145517_InitialDb.Designer.cs
+++ Web_Sever/Web_Server/Migrations/20250324145517_InitialDb.Designer.cs
@@ -12,7 +12,7 @@ using Web_Server.Data;
 namespace Web_Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250324075027_InitialDb")]
+    [Migration("20250324145517_InitialDb")]
     partial class InitialDb
     {
         /// <inheritdoc />
