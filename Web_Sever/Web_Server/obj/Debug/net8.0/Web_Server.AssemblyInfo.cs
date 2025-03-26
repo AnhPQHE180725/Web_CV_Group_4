d0c37bd89a042397e6bc8fbff90b67b4a9a23826
TranTuan66 <he173048tranvantuan@gmail.com>
2025-03-26 22:18:21 +0700

    fix applyjob khi chua login

--- Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
+++ Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0852d2ade5e3380fc9effb5f6ad670df8491322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+810f3ddaaf738074ab6c11ba7e967ac55c4ab111")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
