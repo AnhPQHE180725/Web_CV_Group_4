9d9160711b487d0f8fa1d78a04714709abdfa0d6
TranTuan66 <he173048tranvantuan@gmail.com>
2025-03-28 05:14:13 +0700

    chinh sua giao dien va validate 1 so input

--- Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
+++ Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ccff2b73dafb85ffd591c3a2853992280eb88c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62c9259c17b860858491a597e3c2495dfdf6d77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
