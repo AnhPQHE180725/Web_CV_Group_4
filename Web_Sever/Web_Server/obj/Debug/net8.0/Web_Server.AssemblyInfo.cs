d62c9259c17b860858491a597e3c2495dfdf6d77
TranTuan66 <he173048tranvantuan@gmail.com>
2025-03-28 01:51:33 +0700

    fix mo ta va css cty

--- Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
+++ Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48dfd405169ad530fd4ce7cdd23d96bc12bdb36b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ccff2b73dafb85ffd591c3a2853992280eb88c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
