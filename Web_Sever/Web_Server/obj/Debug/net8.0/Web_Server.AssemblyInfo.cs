1c3b5cb9a4e41209dce4869c04c314a78a2bcd46
Josee0801 <duyanhtranhoang@gmail.com>
2025-03-31 10:58:03 +0700

    add recruiter edit

--- Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
+++ Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2aae381a965becc12758886b02032674b49ba9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444511907d5501b2c77f1db9ae8bfd202926b2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
