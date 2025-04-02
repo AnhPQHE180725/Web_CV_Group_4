89f547f099de955a2c07cce9fe066b8b014b3757
TranTuan66 <he173048tranvantuan@gmail.com>
2025-04-02 19:42:06 +0700

    fix dk cty

--- Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
+++ Web_Sever/Web_Server/obj/Debug/net8.0/Web_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72cb49fcd5b177959673cf4d2d874aa94afa8527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ae910a3ac13f6eb039fb9b2571c05941610c46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
