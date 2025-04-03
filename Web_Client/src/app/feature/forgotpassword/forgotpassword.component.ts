86bb5289d8d2481ba29346e15855659d58a369da
AnhPQHE180725 <anhpqhe180725@fpt.edu.vn>
2025-04-03 10:44:32 +0700

    fix front end

--- Web_Client/src/app/feature/forgotpassword/forgotpassword.component.ts
+++ Web_Client/src/app/feature/forgotpassword/forgotpassword.component.ts
@@ -11,23 +11,34 @@ import { Router } from '@angular/router';
 })
 export class ForgotpasswordComponent {
   email: string ='';
+  isWorking = false; // Trạng thái xử lý
+
   constructor(private authService: AuthService, private router: Router) {}
-    onFormSubmit(): void {
-    if (this.email.trim()) {
-      this.authService.forgotPassword(this.email).subscribe({
-        next: () => {
-          alert('Email đặt lại mật khẩu đã được gửi. Vui lòng kiểm tra email của bạn.');
-          this.router.navigate(['/login']);
-        },
-        error: (err) => {
-          alert('Có lỗi xảy ra hoặc email không tồn tại.');
-          console.error(err);
-        }
-      });
-    } else {
+  onFormSubmit(): void {
+    if (this.isWorking) return; // Nếu đang xử lý, không làm gì cả
+
+    if (!this.email.trim()) {
       alert('Vui lòng nhập email.');
+      return;
     }
+
+    this.isWorking = true; // Bắt đầu xử lý
+
+    this.authService.forgotPassword(this.email).subscribe({
+      next: () => {
+        alert('Email đặt lại mật khẩu đã được gửi. Vui lòng kiểm tra email của bạn.');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        alert('Có lỗi xảy ra hoặc email không tồn tại.');
+        console.error(err);
+      },
+      complete: () => {
+        this.isWorking = false; // Hoàn tất xử lý
+      }
+    });
   }
 }
 
 
+  
